18db06545ac614b42294848a9c50a04b429c1cd3
Kavi <kgonur@ucsd.edu>
2025-12-05 15:14:47 -0800

    Hopefully uploading Class 4 now...

---
+++ Class 4/class4.R
@@ -0,0 +1,17 @@
+#' ---
+#' title: "Class 4"
+#' author: "Kavi Gonur"
+#' date: "October 10, 2025 "
+#' ---
+
+# This is my first R script (not really)
+x <- 1:50
+plot(x)
+plot(x,col="#b53e46")
+plot(x,sin(x),col="#b53e46",type="l",lwd=3)
+
+log(10)
+log(base=10,10)
+log(10,base=2)
+log(10,2)
+log(2,10)
